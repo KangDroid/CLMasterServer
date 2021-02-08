be08b03307ce2d5b61401d0f900a45e7d2770e81
KangDroid <hyunwoo286@gmail.com>
2021-02-08 23:15:53 +0900

    Fix test

--- src/test/kotlin/com/kangdroid/master/service/NodeServiceTest.kt
+++ src/test/kotlin/com/kangdroid/master/service/NodeServiceTest.kt
@@ -4,7 +4,7 @@ import com.kangdroid.master.data.node.NodeRepository
 import com.kangdroid.master.data.node.dto.NodeSaveRequestDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
