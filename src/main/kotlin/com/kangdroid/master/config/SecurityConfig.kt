7bc08a530be7dac92fa3a70a983e1ec6778220d5
KangDroid <hyunwoo286@gmail.com>
2021-02-10 20:34:10 +0900

    Add Security Config For now

---
+++ src/main/kotlin/com/kangdroid/master/config/SecurityConfig.kt
@@ -0,0 +1,17 @@
+package com.kangdroid.master.config
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/**").permitAll()
+    }
+}
