39c079198ccb8ed4a4d42340e832d29fb94873d8
KangDroid <hyunwoo286@gmail.com>
2021-02-22 22:15:25 +0900

    ClientApiControllerTest: Add test for getting node's load

--- src/test/kotlin/com/kangdroid/master/controller/ClientApiControllerTest.kt
+++ src/test/kotlin/com/kangdroid/master/controller/ClientApiControllerTest.kt
@@ -1,10 +1,23 @@
 package com.kangdroid.master.controller
 
+import com.kangdroid.master.config.TestConfiguration
+import com.kangdroid.master.data.node.NodeRepository
+import com.kangdroid.master.data.node.dto.NodeLoadResponseDto
+import com.kangdroid.master.data.node.dto.NodeSaveRequestDto
+import com.kangdroid.master.data.node.dto.NodeSaveResponseDto
+import com.kangdroid.master.data.user.UserRepository
+import com.kangdroid.master.service.NodeService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
@@ -16,5 +29,51 @@ class ClientApiControllerTest {
     @Autowired
     private lateinit var testRestTemplate: TestRestTemplate
 
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var testConfiguration: TestConfiguration
+
+    @Autowired
+    private lateinit var nodeRepository: NodeRepository
+
+    @Autowired
+    private lateinit var nodeService: NodeService
+
     private val baseUrl: String = "http://localhost"
+
+    @Before
+    @After
+    fun clearAllRepo() {
+        nodeRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+    @Test
+    fun isGettingNodeLoadWorking() {
+        // URL
+        val urlFinal: String = "$baseUrl:$port/api/client/node/load"
+        // save node first
+        val nodeSaveRequestDto: NodeSaveRequestDto = NodeSaveRequestDto(
+            id = 10,
+            hostName = "testing",
+            hostPort = testConfiguration.computeNodeServerPort,
+            ipAddress = testConfiguration.computeNodeServerHostName
+        )
+
+        // Save node
+        var returnValue: NodeSaveResponseDto = nodeService.save(nodeSaveRequestDto)
+
+        // Request
+        val responseLoad: ResponseEntity<Array<NodeLoadResponseDto>> = testRestTemplate.getForEntity(urlFinal, Array<NodeLoadResponseDto>::class)
+        assertThat(responseLoad.body).isNotEqualTo(null) // Check for null
+
+        // Get Response Value
+        val responseValue: Array<NodeLoadResponseDto> = responseLoad.body!!
+        for (loadDto in responseValue) {
+            assertThat(loadDto.regionName).isNotEqualTo("")
+            assertThat(loadDto.nodeLoadPercentage).isNotEqualTo("")
+        }
+    }
 }
