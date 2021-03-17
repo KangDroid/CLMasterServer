ddd762d5fa3dd6a3097317f74add1d0f6ea72f0e
KangDroid <hyunwoo286@gmail.com>
2021-03-17 22:04:35 +0900

    NodeService: Add logging system for nodeService

--- src/main/kotlin/com/kangdroid/master/service/NodeService.kt
+++ src/main/kotlin/com/kangdroid/master/service/NodeService.kt
@@ -12,6 +12,8 @@ import com.kangdroid.master.data.node.dto.NodeSaveResponseDto
 import com.kangdroid.master.error.exception.ConflictException
 import com.kangdroid.master.error.exception.NotFoundException
 import com.kangdroid.master.error.exception.UnknownErrorException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -32,6 +34,9 @@ class NodeService {
 
     // Global Rest Template
     lateinit var restTemplate: RestTemplate
+
+    // Default Logger
+    private val logger: Logger = LoggerFactory.getLogger(NodeService::class.java)
 
     @PostConstruct
     fun initRestTemplate() {
@@ -55,18 +60,23 @@ class NodeService {
      * returns: UserImageResponseDto - Containing Error Message.
      */
     fun createContainer(userImageSaveRequestDto: UserImageSaveRequestDto): ResponseEntity<UserImageResponseDto> {
+        logger.info("createContainer started for user: ${userImageSaveRequestDto.userToken}")
         // Find Compute Node information given DTO - to register image on that container.
         val node: Node = nodeRepository.findByRegionName(userImageSaveRequestDto.computeRegion) ?: run {
+            logger.error("Invalid Compute region specified: ${userImageSaveRequestDto.computeRegion} seems like it is not available.")
             throw NotFoundException("Cannot find Compute Region!")
         }
 
         // Request compute-node to create a fresh container
+        logger.info("Requesting to node server...")
         val url: String = "http://${node.ipAddress}:${node.hostPort}/api/node/image"
         val responseEntity: ResponseEntity<UserImageResponseDto> =
             runCatching<ResponseEntity<UserImageResponseDto>> {
                 restTemplate.postForEntity(url, UserImageResponseDto::class.java)
             }.onFailure {
-                println(it.stackTraceToString())
+                logger.error("Requesting to node server failed.")
+                logger.error("The Stack Trace:")
+                logger.error(it.stackTraceToString())
             }.getOrNull() ?: run {
                 throw UnknownErrorException("Cannot communicate with Compute node!")
             }
@@ -75,12 +85,17 @@ class NodeService {
         userImageResponseDto.regionLocation = userImageSaveRequestDto.computeRegion
 
         // Save back to dockerImage DBData
+        logger.info("Saving image information to DB Data")
         val checkResponse: String = userService.saveWithCheck(userImageSaveRequestDto.userToken, userImageResponseDto)
 
         if (checkResponse != "") {
+            logger.error("Error occurred when saving information to DB!")
+            logger.error(checkResponse)
             throw UnknownErrorException(checkResponse)
         }
 
+        logger.info("Successfully created container: ${userImageResponseDto.containerId}")
+        logger.info("Returning entity for user: ${userImageSaveRequestDto.userToken}")
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(userImageResponseDto)
@@ -90,8 +105,13 @@ class NodeService {
      * restartContainer: Restart Container given Docker Image
      */
     fun restartContainer(dockerImage: DockerImage): String {
+        logger.info("Restarting container requested for ${dockerImage.dockerId}")
+
         val node: Node = nodeRepository.findByRegionName(dockerImage.computeRegion)
-            ?: return "Cannot find Compute Region!"
+            ?: run {
+                logger.info("Seems like invalid region is requested: ${dockerImage.computeRegion}")
+                return "Cannot find Compute Region!"
+            }
 
         // Request compute-node to create a fresh container
         val url: String = "http://${node.ipAddress}:${node.hostPort}/api/node/restart"
@@ -101,18 +121,23 @@ class NodeService {
             var containerId: String
         )
 
+        logger.info("Sending restarting request to node server.")
         val requestDto: restartRequestDto = restartRequestDto(dockerImage.dockerId)
 
         val responseEntity: ResponseEntity<String> = runCatching {
             restTemplate.postForEntity(url, requestDto, String::class.java)
         }.onFailure {
-            println(it.stackTraceToString())
+            logger.error("Error occurred when requesting restart on node server.")
+            logger.error(it.stackTraceToString())
         }.getOrNull() ?: return "Cannot communicate with Compute node!"
 
         if (responseEntity.hasBody()) {
+            logger.error("Seems like restarting request somehow failed!")
+            logger.error(responseEntity.body)
             return responseEntity.body!!
         }
 
+        logger.info("Restarting container for ${dockerImage.dockerId} succeed!")
         return ""
     }
 
@@ -137,6 +162,8 @@ class NodeService {
      * returns: A NodeSaveResponseDto, containing errorMessage.
      */
     fun save(nodeSaveRequestDto: NodeSaveRequestDto): ResponseEntity<NodeSaveResponseDto> {
+        logger.info("Node save requested for ${nodeSaveRequestDto.ipAddress}")
+
         // Convert DTO to Entity
         val node: Node = nodeSaveRequestDto.toEntity()
 
@@ -144,17 +171,23 @@ class NodeService {
         node.regionName = "Region-${nodeRepository.count()}"
 
         // Find Any duplicated registered node. - if duplicated node found, return "Error"
+        logger.info("Finding any duplicated region..")
         val nodeGot: Node = nodeRepository.findByIpAddress(node.ipAddress) ?: Node(id = Long.MAX_VALUE, "", "", "", "")
         if (nodeGot.id != Long.MAX_VALUE) {
+            logger.error("Duplicated compute node is found on IP Address: ${node.ipAddress}")
             throw ConflictException("Duplicated Compute Node is found on IP Address: ${node.ipAddress}")
         }
 
         // Check for node integrity
+        logger.info("Checking node integrity")
         val result: Cause = isNodeRunning(nodeSaveRequestDto)
 
         return if (result.cause.isNotEmpty() || result.value) {
+            logger.error("Node failed or network connection exception!")
+            logger.error(result.cause)
             throw UnknownErrorException(result.cause)
         } else {
+            logger.info("Succeed to save node for ${nodeSaveRequestDto.ipAddress}!")
             ResponseEntity
                 .status(HttpStatus.OK)
                 .body(NodeSaveResponseDto(nodeRepository.save(node)))
@@ -169,11 +202,13 @@ class NodeService {
      * returns: false - when any exception occurs[or internal server error].
      */
     private fun isNodeRunning(nodeSaveRequestDto: NodeSaveRequestDto): Cause {
+        logger.info("Checking whether node is actually running..[node: ${nodeSaveRequestDto.ipAddress}]")
         val url: String = "http://${nodeSaveRequestDto.ipAddress}:${nodeSaveRequestDto.hostPort}/api/alive"
         val responseEntity: ResponseEntity<NodeAliveResponseDto> = runCatching {
             restTemplate.getForEntity(url, NodeAliveResponseDto::class.java)
         }.onFailure {
-            println(it.stackTraceToString())
+            logger.error("Error occurred when checking node is running.")
+            logger.error(it.stackTraceToString())
         }.getOrNull() ?: return Cause(
             value = false,
             cause = "Connecting to Node Server failed. Check for IP/Port again."
@@ -181,6 +216,7 @@ class NodeService {
 
         val response: NodeAliveResponseDto = responseEntity.body!!
 
+        logger.info("Checking node running completed for ${nodeSaveRequestDto.ipAddress}!")
         return Cause(
             value = response.isDockerServerRunning,
             cause = response.errorMessage
